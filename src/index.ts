37abf02a18e61f449cd9846804e47b218807ed9c
Brecht Billiet <billietbrecht@gmail.com>
2016-04-14 11:52:45 +0200

    added APP_BASE_HREF

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import {WineCellarApp} from "./common/containers/application.container";
 import {bootstrap} from "angular2/platform/browser";
 import {HTTP_PROVIDERS} from "angular2/http";
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from "angular2/router";
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from "angular2/router";
 import {provide} from "angular2/core";
 bootstrap(WineCellarApp, [
     ROUTER_PROVIDERS,
     HTTP_PROVIDERS,
+    provide(APP_BASE_HREF, {useValue: "/"}),
     provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
