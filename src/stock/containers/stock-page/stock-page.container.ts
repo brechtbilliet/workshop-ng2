e90108f741076913b75b34ba067375ff6a319ed4
Brecht Billiet <billietbrecht@gmail.com>
2016-04-18 19:34:15 +0200

    rating component + number-picker componetn

--- src/stock/containers/stock-page/stock-page.container.ts
+++ src/stock/containers/stock-page/stock-page.container.ts
@@ -3,9 +3,11 @@ import {DefaultPage} from "../../../common/components/default-page/default-page.
 import {Main} from "../../../common/components/main/main.component";
 import {CollapsableSidebar} from "../../../common/containers/collapsable-sidebar/collapsable-sidebar.container";
 import {FavoriteWines} from "../../components/favorite-wines/favorite-wines.component";
+import {NumberPicker} from "../../../common/components/number-picker/number-picker.component";
+import {Rating} from "../../../common/components/rating/rating.component";
 @Component({
     selector: "stock-page",
-    directives: [DefaultPage, Main, CollapsableSidebar, FavoriteWines],
+    directives: [DefaultPage, Main, CollapsableSidebar, FavoriteWines, NumberPicker, Rating],
     template: `
         <default-page>
             <collapsable-sidebar class="hidden-sm hidden-xs">
@@ -15,6 +17,8 @@ import {FavoriteWines} from "../../components/favorite-wines/favorite-wines.comp
                 <div class="row">
                     <div class="col-sm-12">
                         <h2><i class="fa fa-user"></i>&nbsp;My wines <span class="badge badge-primary">5</span></h2>
+                        <number-picker [amount]="5" (setAmount)="setAmount($event)"></number-picker>
+                        <rating [rating]="2" (setRate)="setRate($event)" big="true"></rating>
                     </div>
                 </div>
             </main>
@@ -22,4 +26,10 @@ import {FavoriteWines} from "../../components/favorite-wines/favorite-wines.comp
      `
 })
 export class StockPage {
+    public setAmount(number: Number): void {
+        console.log(number);
+    }
+    public setRate(number: Number): void {
+        console.log(number);
+    }
 }
