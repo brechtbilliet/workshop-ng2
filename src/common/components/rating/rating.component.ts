d2919dd8c464aa58774b0625adc88bf83ea468f6
Brecht Billiet <billietbrecht@gmail.com>
2016-04-23 08:59:47 +0200

    rating & number-picker component

--- src/common/components/rating/rating.component.ts
+++ src/common/components/rating/rating.component.ts
@@ -1,14 +1,41 @@
-import {Component} from "angular2/core";
+import {Component, EventEmitter, Output, Input} from "angular2/core";
 @Component({
     selector: "rating",
     styles: [require("./rating.component.scss")],
     template: `
-        <i class="fa fa-star rating"></i>
-        <i class="fa fa-star rating"></i>
-        <i class="fa fa-star rating"></i>
-        <i class="fa fa-star rating"></i>
-        <i class="fa fa-star rating"></i>
-        `
+        <i class="fa fa-star rating" [class.fa-2x]="big" *ngFor="#i of [1,2,3,4,5]"
+            [class.over]="overValue >= i" 
+            [class.starred]="rating >= i" 
+            (mouseover)="over(i)" 
+            (mouseout)="out()" 
+            (click)="update(i)"></i>
+    `
 })
 export class Rating {
+    @Input()
+    public rating: number;
+
+    @Input()
+    public big: boolean;
+
+    @Output()
+    public setRate: EventEmitter<number>;
+
+    public overValue: number;
+
+    constructor() {
+        this.setRate = new EventEmitter();
+    }
+
+    public update(value: number): void {
+        this.setRate.emit(value);
+    }
+
+    public over(value: number): void {
+        this.overValue = value;
+    }
+
+    public out(): void {
+        this.overValue = 0;
+    }
 }
