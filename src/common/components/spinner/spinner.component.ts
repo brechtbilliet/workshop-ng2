81dc47df6d21115b203b3abc116cb55f5da34330
Brecht Billiet <billietbrecht@gmail.com>
2016-04-23 11:59:04 +0200

    spinner

--- src/common/components/spinner/spinner.component.ts
+++ src/common/components/spinner/spinner.component.ts
@@ -1,13 +1,15 @@
-import {Component} from "angular2/core";
+import {Component, Input} from "angular2/core";
 @Component({
     selector: "spinner",
     styles: [require("./spinner.component.scss")],
     template: `
-        <div class="spinner active">
+        <div class="spinner" [class.active]="spin">
             <div class="bounce1"></div>
             <div class="bounce2"></div>
             <div class="bounce3"></div>
         </div>`
 })
 export class Spinner {
+    @Input()
+    public spin: boolean;
 }
