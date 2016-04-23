a774dd91a8bb929475b715ef9be197544933fe67
Brecht Billiet <billietbrecht@gmail.com>
2016-04-23 13:57:02 +0200

    stockpage finished

--- src/common/containers/collapsable-sidebar/collapsable-sidebar.container.ts
+++ src/common/containers/collapsable-sidebar/collapsable-sidebar.container.ts
@@ -1,15 +1,28 @@
 import {Component} from "angular2/core";
+import {Observable} from "rxjs/Observable";
+import {ApplicationState} from "../../state/ApplicationState";
+import {CONTAINER_COLLAPSABLESIDEBAR_TOGGLE} from "../../actionTypes";
+import {Store} from "@ngrx/store";
 @Component({
     selector: "collapsable-sidebar",
     styles: [require("./collapsable-sidebar.container.scss")],
     template: `
-        <div class="collapsable-part" >
-            <button class="btn btn-primary btn-collapsable">
-                <i class="fa fa-chevron-left"></i>
+        <div class="collapsable-part" [class.is-collapsed]="isCollapsed$|async">
+            <button class="btn btn-primary btn-collapsable" (click)="toggleSidebar()">
+                <i class="fa" [class.fa-chevron-right]="isCollapsed$| async" [class.fa-chevron-left]="(isCollapsed$| async) === false"></i>
             </button>
-            <ng-content></ng-content>
+            <ng-content *ngIf="(isCollapsed$| async) === false"></ng-content>
         </div>
     `
 })
 export class CollapsableSidebar {
+    public isCollapsed$: Observable<boolean>;
+
+    constructor(private store: Store<ApplicationState>) {
+        this.isCollapsed$ = store.select((state: ApplicationState) => state.containers.collapsableSidebar.isCollapsed);
+    }
+
+    public toggleSidebar(): void {
+        this.store.dispatch({type: CONTAINER_COLLAPSABLESIDEBAR_TOGGLE});
+    }
 }
--- src/stock/containers/stock-page/stock-page.container.ts
+++ src/stock/containers/stock-page/stock-page.container.ts
@@ -1,35 +1,95 @@
-import {Component} from "angular2/core";
+import {Component, OnInit, ElementRef} from "angular2/core";
 import {DefaultPage} from "../../../common/components/default-page/default-page.component";
 import {Main} from "../../../common/components/main/main.component";
 import {CollapsableSidebar} from "../../../common/containers/collapsable-sidebar/collapsable-sidebar.container";
 import {FavoriteWines} from "../../components/favorite-wines/favorite-wines.component";
-import {NumberPicker} from "../../../common/components/number-picker/number-picker.component";
-import {Rating} from "../../../common/components/rating/rating.component";
+import {ApplicationState} from "../../../common/state/ApplicationState";
+import {Store} from "@ngrx/store";
+import {WineEndpoint} from "../../endpoints/WineEndpoint";
+import {Wine} from "../../entities/Wine";
+import {Observable} from "rxjs/Observable";
+import {WineResults} from "../../components/wine-results/wine-results.component";
+import * as _ from "lodash";
+import {ROUTER_DIRECTIVES} from "angular2/router";
 @Component({
     selector: "stock-page",
-    directives: [DefaultPage, Main, CollapsableSidebar, FavoriteWines, NumberPicker, Rating],
+    providers: [WineEndpoint],
+    directives: [ROUTER_DIRECTIVES, DefaultPage, Main, CollapsableSidebar, FavoriteWines, WineResults],
     template: `
         <default-page>
             <collapsable-sidebar class="hidden-sm hidden-xs">
-                <favorite-wines></favorite-wines>
+                 <favorite-wines (onSetStock)="onSetStock($event)" [wines]="wines$ | async"></favorite-wines>
             </collapsable-sidebar>
             <main>
                 <div class="row">
+                    <div class="col-sm-8">
+                        <div class="input-group">
+                            <input type="text" class="form-control input-lg"/>
+                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <a [routerLink]="['AddWine']" class="btn btn-primary btn-lg btn-block">
+                            <i class="fa fa-plus-circle"></i>&nbsp;Add
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-sm-12">
-                        <h2><i class="fa fa-user"></i>&nbsp;My wines <span class="badge badge-primary">5</span></h2>
-                        <number-picker [amount]="5" (setAmount)="setAmount($event)"></number-picker>
-                        <rating [rating]="2" (setRate)="setRate($event)" big="true"></rating>
+                        <h2>
+                            <i class="fa fa-user"></i>&nbsp;My wines 
+                            <span class="badge badge-primary">{{numberOfWines$|async}}</span>
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <wine-results 
+                            [wines]="matchingWines$| async" 
+                            (onRemove)="remove($event)" 
+                            (onSetRate)="onSetRate($event)" 
+                            (onSetStock)="onSetStock($event)">
+                        </wine-results>
                     </div>
                 </div>
             </main>
         </default-page>
      `
 })
-export class StockPage {
-    public setAmount(number: Number): void {
-        console.log(number);
+export class StockPage implements OnInit {
+    public wines$: Observable<Array<Wine>>;
+    public matchingWines$: Observable<Array<Wine>>;
+    public numberOfWines$: Observable<Number>;
+
+    constructor(private el: ElementRef, private store: Store<ApplicationState>, private wineEndpoint: WineEndpoint) {
+        this.wineEndpoint.load();
+        this.wines$ = this.store.select((state: ApplicationState) => state.data.wines);
     }
-    public setRate(number: Number): void {
-        console.log(number);
+
+    public remove(wine: Wine): void {
+        this.wineEndpoint.remove(wine);
+    }
+
+    public onSetRate(item: any): void {
+        this.wineEndpoint.setRate(item.wine, item.value);
+    }
+
+    public onSetStock(item: any): void {
+        this.wineEndpoint.setStock(item.wine, item.value);
+    }
+
+    public ngOnInit(): void {
+        let input$: Observable<string> = Observable.fromEvent(this.el.nativeElement.querySelector("input"), "keyup")
+            .map((e: any) => e.target.value).startWith("");
+
+        this.matchingWines$ = Observable.combineLatest(input$, this.wines$)
+            .map((resp: [string, Array<Wine>]) => {
+                return resp[1].filter((wine: Wine) => wine.name.toLowerCase().indexOf(resp[0].toLowerCase()) > -1);
+            });
+        this.numberOfWines$ = this.wines$.map((wines: Array<Wine>) => {
+            return _.sumBy(wines, (wine: Wine) => {
+                return wine.inStock;
+            });
+        });
     }
 }
